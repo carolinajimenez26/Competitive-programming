e7c87a7d82f05577dea5848dae61715a8cb04fa6
Carolina <carolina.jimenez.g@gmail.com>
2019-09-10 21:34:16 -0300

    Adds another solution for Buy and Sell Stock problem

--- LeetCode/best-time-to-buy-and-sell-stock.cpp
+++ LeetCode/best-time-to-buy-and-sell-stock.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// Time complexity: O(n^2)
+// Space complexity: O(1)
 int maxProfit(vector<int>& prices) {
   int max_profit = 0;
 
@@ -14,6 +16,31 @@ int maxProfit(vector<int>& prices) {
     }
   }
   return max_profit;
+}
+
+// Time complexity: O(n log n)
+// Space complexity: O(n)
+int maxProfit2(vector<int>& prices) {
+  if (prices.size() == 0) return 0;
+  int last = prices.size() - 1;
+  
+  multiset<int> elems;
+  // log n
+  for (int i = last; i >= 0; i--) {
+    elems.insert(prices[i]);
+  }
+
+  int best = 0;
+  for (int i = last; i >= 1; i--) { // n
+    int curr = prices[i];
+    auto it = elems.find(curr); // log n
+    elems.erase(it);
+    int min_n = *elems.begin();
+    if (curr - min_n > best) {
+      best = curr - min_n;
+    }
+  }
+  return best;
 }
 
 int main(void) {
