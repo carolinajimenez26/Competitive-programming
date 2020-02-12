68dec2b3fc7609709eb45f77dc6a390bd9ba1ff7
Carolina <carolina.jimenez.g@gmail.com>
2020-02-12 12:18:47 -0300

    Adds Subarray Sum Equals K problem

---
+++ LeetCode/subarray-sum-equals-k.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+  int GetContinuousSum(const vector<int>& nums, int from, int k) {
+    int sum = 0, result = 0;
+    sum += nums[from];
+    for (int i = from + 1; i < nums.size(); i++) {
+      sum += nums[i];
+      if (sum == k) {
+        result++;
+      }
+    }
+    return result;
+  }
+  int getCountElementsEquals(const vector<int>& nums, int k) {
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (nums[i] == k) {
+        sum++;
+      }
+    }
+    return sum;
+  }
+public:
+  // O (n ** 2), where n = nums.size()
+  int subarraySum(vector<int>& nums, int k) {
+    int result = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      result += GetContinuousSum(nums, i, k);
+    }
+    return result + getCountElementsEquals(nums, k);
+  }
+};
+
+struct Test {
+  vector<int> nums;
+  int k;
+  int result;
+};
+
+int main(void) {
+  vector<Test> tests = {
+    {
+      {1,1,1},
+      2,
+      2
+    },
+    {
+      {0,0,1,1,0,0,1},
+      2,
+      10 // {0,3}, {0,4}, {0,5}, {1,3}, {1,4}, {1,5}, {2,3}, {2,4}, {2,5}, {3,6}
+    },
+    {
+      {1,-1,1},
+      1,
+      3
+    },
+    {
+      {1,-1,1,1},
+      1,
+      5
+    }
+  };
+  int tc = 0;
+  bool succeed = true;
+
+  for (auto test : tests) {
+    Solution sol;
+    int out = sol.subarraySum(test.nums, test.k);
+    if (out != test.result) {
+      succeed = false;
+      cout << "Failed on test #" << tc << endl;
+      cout << "Got " << out << ", expected " << test.result << endl;
+    }
+    tc++;
+  }
+
+  if (succeed) cout << "Good job" << endl;
+  else cout << "Keep trying" << endl;
+
+  return 0;
+}
