7d6898f5dded49ea1a6e357db101754feed51bbe
Carolina <carolina.jimenez.g@gmail.com>
2020-04-26 16:32:43 +0200

    Adds Longest Common Subsequence problem

---
+++ LeetCode/30-day-leetcoding-challenge/26.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+  vector<vector<int>> memo;
+public:
+  int longestCommonSubsequence(const string& text1, const string& text2, int idx1, int idx2) {
+    if (idx1 >= text1.size() || idx2 >= text2.size()) {
+      return 0;
+    }
+    if (memo[idx1][idx2] != -1) {
+      return memo[idx1][idx2];
+    }
+    int result = 0;
+    if (text1[idx1] == text2[idx2]) { // match
+      result = 1 + longestCommonSubsequence(text1, text2, idx1 + 1, idx2 + 1);
+    } else {
+      int op1 = longestCommonSubsequence(text1, text2, idx1 + 1, idx2);
+      int op2 = longestCommonSubsequence(text1, text2, idx1, idx2 + 1);
+      result = max(op1, op2);
+    }
+    memo[idx1][idx2] = result;
+    return result;
+  }
+
+  int longestCommonSubsequence(string text1, string text2) {
+    memo.assign(text1.size(), vector<int>(text2.size(), -1));
+    return longestCommonSubsequence(text1, text2, 0, 0);
+  }
+};
+
+int main(void) {
+  return 0;
+}
