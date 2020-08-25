3b47dd34df5ae3ce74de31245207d87942b60c93
Carolina <carolina.jimenez.g@gmail.com>
2020-08-25 15:44:59 +0200

    Refactors Minimum Cost For Tickets. Made it more readable using State structure.

--- LeetCode/august-challenge/25.cpp
+++ LeetCode/august-challenge/25.cpp
@@ -1,43 +1,57 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <unordered_map>
+#include <map>
 #include <limits>
 using namespace std;
+
+struct State {
+  int day, passes;
+
+  bool operator<(const State& other) const {
+    if (day == other.day) {
+      return passes < other.passes;
+    }
+    return day < other.day;
+  }
+};
 
 class Solution {
 private:
   unordered_set<int> days_to_travel;
   vector<int> costs;
-  vector<vector<int>> memo;
+  // TODO: change map for unordered map to improve runtime access. For this, we need
+  // to create a hash function for State
+  map<State, int> memo;
   const int INF = numeric_limits<int>::max();
 
-  int mincostTickets(int day, int curr_pass_amount) {
-    if (day > 365) return 0;
-    if (memo[day][curr_pass_amount] != -1) return memo[day][curr_pass_amount];
+  int mincostTickets(const State& state) {
+    if (state.day > 365) return 0;
+    if (memo.count(state) > 0) return memo[state];
 
-    if (days_to_travel.count(day) == 0) { // this is not a day I need to travel
-      if (curr_pass_amount > 0) { // passes need to be use each continuous day
-        return memo[day][curr_pass_amount] = mincostTickets(day + 1, curr_pass_amount - 1);
+    if (days_to_travel.count(state.day) == 0) { // this is not a day I need to travel
+      if (state.passes > 0) { // passes need to be use each continuous day
+        return memo[state] = mincostTickets({state.day + 1, state.passes - 1});
       } 
-      return memo[day][curr_pass_amount] = mincostTickets(day + 1, curr_pass_amount);
+      return memo[state] = mincostTickets({state.day + 1, state.passes});
     }
-    if (curr_pass_amount > 0) { // we can use one of the passes we already bought
-      return memo[day][curr_pass_amount] = mincostTickets(day + 1, curr_pass_amount - 1);
+    if (state.passes > 0) { // we can use one of the passes we already bought
+      return memo[state] = mincostTickets({state.day + 1, state.passes - 1});
     }
 
-    int op1 = costs[0] + mincostTickets(day + 1, 0); // we use the pass today
-    int op2 = costs[1] + mincostTickets(day + 1, 6); // we use one pass today, we have 6 left
-    int op3 = costs[2] + mincostTickets(day + 1, 29); // we use one pass today, we have 29 left
+    int op1 = costs[0] + mincostTickets({state.day + 1, 0}); // we use the pass today
+    int op2 = costs[1] + mincostTickets({state.day + 1, 6}); // we use one pass today, we have 6 left
+    int op3 = costs[2] + mincostTickets({state.day + 1, 29}); // we use one pass today, we have 29 left
 
-    return memo[day][curr_pass_amount] = min(op1, min(op2, op3));
+    return memo[state] = min(op1, min(op2, op3));
   }
 
 public:
   int mincostTickets(vector<int>& days, vector<int>& _costs) {
     days_to_travel = unordered_set<int>(days.begin(), days.end());
     costs = _costs;
-    memo.resize(367, vector<int>(30 * 367, -1));
-    return mincostTickets(1, 0);
+    return mincostTickets({1, 0});
   }
 };
 
