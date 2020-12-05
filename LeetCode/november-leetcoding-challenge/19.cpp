cf0b38457d821d3af39d80b20ba3b54bfb9022dc
Carolina <carolina.jimenez.g@gmail.com>
2020-12-05 17:47:46 +0100

    Add Merge Intervals problem

--- LeetCode/november-leetcoding-challenge/19.cpp
+++ LeetCode/november-leetcoding-challenge/19.cpp
@@ -1,61 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <set>
 using namespace std;
-using interval_vector = vector<vector<int>>;
 
 class Solution {
-private:
-  bool Overlaps(const vector<int>& a, const vector<int>& b) {
-    return max(a[0], b[0]) <= min(a[1], b[1]);
-  }
-
-  vector<int> Merge(const vector<int>& a, const vector<int>& b) {
-    int left = min(a[0], b[0]);
-    int right = max(a[1], b[1]);
-    vector<int> new_interval = {left, right};
-    return new_interval;
-  }
-
-  void RemoveAll(set<vector<int>>& from, const interval_vector& to_remove) {
-    for (auto interval : to_remove) {
-      from.erase(interval);
-    }
-  }
 public:
-  // where n = intervals.size()
-  // Runtime: n^2
-  // Spacetime: n
-  interval_vector merge(interval_vector& intervals) {
-    if (intervals.empty()) return {};
-    set<vector<int>> ans;
-    for (auto interval : intervals) { // O(n)
-      int left = interval[0];
-      int right = interval[1];
-      interval_vector to_remove;
-      for (auto interval_ans : ans) { // O(n)
-        if (Overlaps(interval, interval_ans)) {
-          to_remove.push_back(interval_ans);
-          left = min(left, interval_ans[0]);
-          right = max(right, interval_ans[1]);
-        }
-      }
-      RemoveAll(ans, to_remove); // O(n * log n)
-      ans.insert({left, right});
-    }
-    return interval_vector(ans.begin(), ans.end());
-  }
-
-  void Print(const interval_vector& intervals) {
+  void Print(const vector<vector<int>>& intervals) {
     for (auto interval : intervals) {
       cout << "{" << interval[0] << "," << interval[1] << "}, ";
     }
     cout << endl;
   }
+
+  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    int MAX = 10e4 + 4;
+    vector<int> positives(MAX, 0);
+    vector<int> negatives(MAX, 0);
+    for (auto interval : intervals) {
+      int from = interval[0], to = interval[1];
+      positives[from] += 1;
+      negatives[to + 1] -= 1;
+    }
+    int curr = 0;
+    vector<int> tmp;
+    tmp.reserve(MAX);
+    for (int i = 0; i < MAX; i++) {
+      // close events first 
+      if (curr > 0 && (curr + negatives[i]) == 0) {
+        tmp.push_back(i - 1);
+      }
+      curr += negatives[i];
+
+      // open second
+      if (curr == 0 && (curr + positives[i]) > 0) {
+        tmp.push_back(i);
+      }
+      curr += positives[i];
+    }
+    vector<vector<int>> ans;
+    for (int i = 0; i < tmp.size(); i += 2) {
+      ans.push_back({ tmp[i], tmp[i + 1] });
+    }
+    return ans;
+  }
 };
 
 struct Test {
-  interval_vector intervals, expected;
+  vector<vector<int>> intervals, expected;
 };
 
 int main(void) {
@@ -80,12 +70,38 @@ int main(void) {
       {{0,22},{1,3},{6,6},{8,10},{15,18},{20,20}},
       {{0,22}}
     },
+    {
+      {{0,3},{4,7},{8,11},{12,15},{5,15}},
+      {{0,3},{4,15}},
+    },
+    {
+      {{0,3},{4,7},{8,11},{12,15},{0,15}},
+      {{0,15}},
+    },
+    {
+      {{0,3},{4,7},{8,11},{12,15},{0,15},{16,20}},
+      {{0,15},{16,20}},
+    },
+    {
+      {{0,3},{4,7},{8,11},{12,15},{0,15},{16,17},{16,20}},
+      {{0,15},{16,20}},
+    },
+    {
+      {{1,4},{0,2},{3,5}},
+      {{0,5}},
+    },
+    {
+      {{4,5},{2,4},{4,6},{3,4},{0,0},{1,1},{3,5},{2,2}},
+      {{0,0},{1,1},{2,6}},
+    }
   };
   int tc = 0;
   bool succeed = true;
   for (auto test : tests) {
     Solution sol;
-    interval_vector out = sol.merge(test.intervals);
+    vector<vector<int>> out = sol.merge(test.intervals);
+    sort(out.begin(), out.end());
+    sort(test.expected.begin(), test.expected.end());
     if (out != test.expected) {
       cout << "Failed on test #" << tc << endl;
       cout << "Input: " << endl;
